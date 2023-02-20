685cb3a22291414369432723390756cd2bf519fc
AlekseyWMZ <AlekseyWMZ@gmail.com>
2023-02-20 06:36:40 +0200

    ...

--- postcss.config.js
+++
@@ -1,4 +0,0 @@
-import tailwindcss from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
-
-export default { tailwindcss, autoprefixer };
--- src/gst/helpers.js
+++ src/gst/helpers.js
@@ -1,5 +1,4 @@
-import { faker } from "@faker-js/faker";
-
+import { faker } from "@faker-js/faker"
 
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
